c91fb2d3b95119eea9f57f9ab6b1383045aa662c
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2022-01-06 19:20:18 +0530

    Modified some constants

--- Type.py
+++ Type.py
@@ -29,7 +29,7 @@ def start_typing(url: str, xpath_to_text_field: str, message: str, type_speed: i
         text_field.send_keys(character)
         time.sleep(waiting_time)
 
-    time.sleep(0.5)
+    time.sleep(2)
     text_field.send_keys(Keys.ENTER)
 
     time.sleep(2)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     message = input("Enter the text you want to type: ")
 
     # Type Speed in Characters per second
-    type_speed = 100
+    type_speed = 30
 
     url = 'https://www.google.com/'
     xpath_to_text_field = '/html[1]/body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[2]/div[2]/input[1]'
